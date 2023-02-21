1de0de383ccde08e65d908b64320c68e4829b76a
Creshent <dobao98123@gmail.com>
2023-02-21 14:08:08 -0500

    Change comments in documentation in interface.

--- Command.java
+++ Command.java
@@ -1,3 +1,10 @@
+/**
+ * This interface acts as a command action, where child classes
+ * implement the action to execute a stopwatch timer.
+ * 
+ * @author Scott Do (Reshlynt)
+ * @version 1.0
+ */
 public interface Command {
     public void execute();
 }
