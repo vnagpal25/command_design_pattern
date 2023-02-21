0e566c3412adf98252ef4351754436080f8ab97e
Vansh Nagpal <2002vansh@gmail.com>
2023-02-21 13:56:13 -0500

    finished command interface + inputhandler ctor

--- InputHandler.java
+++ InputHandler.java
@@ -1,3 +1,15 @@
+import java.util.HashMap;
+
 public class InputHandler {
-  
+  private HashMap<String, Command> commands = new HashMap<String, Command>();
+
+  public InputHandler(StopWatch watch){
+    commands.put("1", OneMinCommand);
+    commands.put("5", FiveMinCommand);
+    commands.put("30", ThirtySecCommand);
+  }
+
+  public boolean inputEntered(String data) {
+
+  }
 }
