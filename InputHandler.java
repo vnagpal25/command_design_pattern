52448bdcbcf6e798a278fee09b7686bae1a94c8d
Vansh Nagpal <2002vansh@gmail.com>
2023-02-21 14:20:06 -0500

    javadoc + inputEntered method

--- InputHandler.java
+++ InputHandler.java
@@ -1,15 +1,32 @@
 import java.util.HashMap;
 
+/**
+ * @author vnagpal
+ *         InputHandler class
+ */
 public class InputHandler {
+  // initializes hashmap
   private HashMap<String, Command> commands = new HashMap<String, Command>();
 
-  public InputHandler(StopWatch watch){
-    commands.put("1", OneMinCommand);
-    commands.put("5", FiveMinCommand);
-    commands.put("30", ThirtySecCommand);
+  /**
+   * Constructor matches inputs to commands
+   * 
+   * @param watch given stopwatch that will be given to the commands
+   */
+  public InputHandler(StopWatch watch) {
+    commands.put("1", new OneMinCommand(watch));
+    commands.put("5", new FiveMinCommand(watch));
+    commands.put("30", new ThirtySecCommand(watch));
   }
 
+  /**
+   * Method validates input for stopwatch
+   * @param data input entered in command line
+   * @return boolean based on valid input 1, 5, or 30
+   */
   public boolean inputEntered(String data) {
-
+    return data.equals("1")
+        || data.equals("5")
+        || data.equals("30");
   }
 }
