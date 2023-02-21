1a57c538d0207240938bb1580fe32e44ec999601
jagger tanner <jrtanner@email.sc.edu>
2023-02-21 14:03:05 -0500

    added helper method

--- StopWatch.java
+++ StopWatch.java
@@ -4,21 +4,29 @@ import java.util.concurrent.TimeUnit;
  * @author Jagger
  */
 public class StopWatch {
+    /**
+     * Creates a new StopWatch
+     */
     public StopWatch() {
         
     }
     /**
-     * Starts a timer for one minute
+     * A helper method to convert seconds to minutes and seconds and then count down
+     * @param time
      */
-    public void countDownOneMin() {
-        for (int i = 60; i >= 0; i--) {
+    private void helper(int time) {
+        for (int i = time; i >= 0; i--) {
+            int minutes = i / 60;
+            int seconds = i % 60;
             System.out.print("\033[H\033[2J");
             System.out.flush();
-            System.out.print("00:");
-            if (i < 10) {
-                System.out.println("0" + i);
+            System.out.print("0"+minutes+":");
+            if (i == 0) {
+                System.out.println("00");
+            } else if (i < 10) {
+                System.out.println("0" + seconds);
             } else {
-                System.out.println(i);
+                System.out.println(seconds);
             }
             try {
                 TimeUnit.MILLISECONDS.sleep(1000);
@@ -26,45 +34,25 @@ public class StopWatch {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    /**
+     * Starts a timer for one minute
+     */
+    public void countDownOneMin() {
+        helper(60);
     }
     /**
      * Starts a timer for five minutes
      */
     public void countDownFiveMin() {
-        for (int i = 300; i >= 0; i--) {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-            System.out.print("00:");
-            if (i < 10) {
-                System.out.println("0" + i);
-            } else {
-                System.out.println(i);
-            }
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        helper(300);
     }
     /**
      * Starts a timer for thirty seconds
      */
     public void countDownThirtySec() {
-        for (int i = 30; i >= 0; i--) {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-            System.out.print("00:");
-            if (i < 10) {
-                System.out.println("0" + i);
-            } else {
-                System.out.println(i);
-            }
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        helper(30);
     }
 }
