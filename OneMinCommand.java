1ade6dc93e7e1e1c6f4916344fab8bc77c852e70
Stewys <adamstewart_7@outlook.com>
2023-02-21 13:57:36 -0500

    one minute command with javadoc

--- OneMinCommand.java
+++ OneMinCommand.java
@@ -1,11 +1,18 @@
 public class OneMinCommand implements Command {
     private StopWatch watch;
     
+    /**
+     * OneMinCommand constructor
+     * @param watch
+     */
     public OneMinCommand(StopWatch watch) {
         this.watch = watch;
     }
     
+    /**
+     * Execute the command
+     */
     public void execute() {
-        watch.oneMin();
+        this.watch.countDownOneMin();
     }
 }
