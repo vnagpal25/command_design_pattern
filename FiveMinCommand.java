43009ecf80acfbcacf9f7282ecc1429a6d89b868
Creshent <dobao98123@gmail.com>
2023-02-21 14:03:31 -0500

    Implementing FiveMinCommand class

--- FiveMinCommand.java
+++ FiveMinCommand.java
@@ -1,16 +1,20 @@
 /**
- * This class create a stopwatch that will countdown
- * from 4:59 to 0:00.
+ * This class create a stopwatch that will countdown from 4:59 to 0:00.
+ * 
  * @author Scott Do (Reshlynt)
  * @version 1.0
  */
 public class FiveMinCommand implements Command {
     private StopWatch watch;
-    public FiveMinCommand (StopWatch watch) {
-        
+
+    public FiveMinCommand(StopWatch watch) {
+        this.watch = watch;
     }
-    @Override
+    /**
+     * This methods run the method that counts down five min
+     * from 04:59, whose method appears in the StopWatch class.
+     */
     public void execute() {
-        
+        watch.countDownFiveMin();
     }
 }
